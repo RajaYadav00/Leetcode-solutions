0a350fcf50c84d8c162cd99980dcda0341973847
Raja Ram Yadav <99987026+RajaYadav00@users.noreply.github.com>
2022-08-11 00:00:02 +0530

    Time: 307 ms (63.53%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,2 +1,23 @@
 # Write your MySQL query statement below
-select name,population,area from world WHERE area>=3000000 OR population>=25000000 ;
+
+
+select name,population,area from world where area>=3000000 or population>=25000000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select name,population,area from world WHERE area>=3000000 OR population>=25000000 ;
